253d1578d1e50ae134ca6bf1e94762b6b303b11f
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-11-05 00:27:38 +0100

    refactor(test): migrate store definition to before()

--- test/test.js
+++ test/test.js
@@ -4,11 +4,15 @@ var expect = require('chai').expect;
 describe('dip test suite', function () {
   'use strict';
 
-  it('should ...', function (done) {
-    var store = {
+  var store;
+
+  before(function () {
+    store = {
       foo: 'bar'
     };
+  });
 
+  it('should ...', function (done) {
     var _dip = new Dip({store: store});
 
     expect(_dip.get('foo')).to.equal('bar');
