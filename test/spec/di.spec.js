665861e0cc07cf5c4d4d4a4007c4c9fd2cca01b2
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-11-08 15:46:00 +0100

    refactor(test): reorganize between getter and setter

--- test/spec/di.spec.js
+++ test/spec/di.spec.js
@@ -11,51 +11,54 @@ describe('dip test suite', function () {
     dip = new Dip({store: store});
   });
 
-  it('should return the correct value on get', function (done) {
-    // default value
-    store.db.foo = {
-      value: 'bar'
-    };
+  describe('getter', function () {
+    it('should return the correct value on get', function (done) {
+      // default value
+      store.db.foo = {
+        value: 'bar'
+      };
 
-    dip.getItem('foo').should.eventually.equal('bar').notify(done);
-  });
+      dip.getItem('foo').should.eventually.equal('bar').notify(done);
+    });
 
-  it('should set the value in the store', function (done) {
-    dip.setItem('john', 'doe').then(function () {
-      dip.getItem('john').should.eventually.equal('doe').notify(done);
+    it('should reject on null key', function (done) {
+      var resolvedFn = sinon.spy();
+      var rejectedFn = sinon.spy();
+
+      dip.getItem(null)
+        .then(resolvedFn)
+        .catch(function () {
+          rejectedFn();
+        })
+        .then(function () {
+          resolvedFn.called.should.be.false;
+          rejectedFn.called.should.be.true;
+          done();
+        });
+
+    });
+
+    it('should reject on undefined key', function (done) {
+      var resolvedFn = sinon.spy();
+      var rejectedFn = sinon.spy();
+
+      // equivalent to dip.getItem(), but explained here for documentation purpose
+      dip.getItem(undefined)
+        .then(resolvedFn)
+        .catch(rejectedFn)
+        .then(function () {
+          resolvedFn.called.should.be.false;
+          rejectedFn.called.should.be.true;
+          done();
+        });
     });
   });
 
-  it('should reject on null key', function (done) {
-    var resolvedFn = sinon.spy();
-    var rejectedFn = sinon.spy();
-
-    dip.getItem(null)
-      .then(resolvedFn)
-      .catch(function () {
-        rejectedFn();
-      })
-      .then(function () {
-        resolvedFn.called.should.be.false;
-        rejectedFn.called.should.be.true;
-        done();
+  describe('setter', function () {
+    it('should set the value in the store', function (done) {
+      dip.setItem('john', 'doe').then(function () {
+        dip.getItem('john').should.eventually.equal('doe').notify(done);
       });
-
+    });
   });
-
-  it('should reject on undefined key', function (done) {
-    var resolvedFn = sinon.spy();
-    var rejectedFn = sinon.spy();
-
-    // equivalent to dip.getItem(), but explained here for documentation purpose
-    dip.getItem(undefined)
-      .then(resolvedFn)
-      .catch(rejectedFn)
-      .then(function () {
-        resolvedFn.called.should.be.false;
-        rejectedFn.called.should.be.true;
-        done();
-      });
-  });
-
 });
